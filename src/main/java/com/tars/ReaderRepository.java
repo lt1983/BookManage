f7f75c21ac48c8b2790f8d981417732da4ab1b8e
Tars.Liu <liutao@seuic.com>
2017-03-30 13:55:15 +0800

    完成3.1章节编码

---
+++ src/main/java/com/tars/ReaderRepository.java
@@ -0,0 +1,11 @@
+package com.tars;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+/**
+ * Created by Tars on 2017/3/30.
+ */
+public interface ReaderRepository extends JpaRepository<Reader,String> {
+}
